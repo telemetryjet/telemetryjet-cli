66eca01da86a2ea463797424232b42e2e6491a10
Chris Dalke <chrisdalke@gmail.com>
2019-10-23 22:06:40 -0400

    Got libserialport building, created simple serial port list code

--- src/deviceManager.cpp
+++ src/deviceManager.cpp
@@ -2,4 +2,22 @@
 
 void DeviceManager::pollDevices() {
     std::cout << "Polling devices...\n";
+
+    uint64_t startTime = (system_clock::now().time_since_epoch()).count();
+
+    struct sp_port** ports;
+    sp_return        listPortResult = sp_list_ports(&ports);
+
+    if (listPortResult == SP_OK) {
+        std::cout << "== Serial Ports ==\n";
+        for (int i = 0; ports[i]; i++) {
+            std::cout << i << ") " << sp_get_port_name(ports[i]) << "\n";
+        }
+        sp_free_port_list(ports);
+    } else {
+        std::cout << "Found no serial ports!\n";
+    }
+    uint64_t delta = (system_clock::now().time_since_epoch()).count() - startTime;
+
+    std::cout << "took " << delta / 1000.0f << "ms \n\n";
 }
--- src/deviceManager.hpp
+++ src/deviceManager.hpp
@@ -1,9 +1,13 @@
 #ifndef H_DEVICE_MANAGER
 #define H_DEVICE_MANAGER
 
+#include <chrono>
+#include <cstdint>
 #include <fmt/format.h>
 #include <iostream>
+#include <libserialport.h>
 #include <string>
+using namespace std::chrono;
 
 class DeviceManager {
 private:
