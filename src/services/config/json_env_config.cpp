b9125af4bc4cefe6be01b46f20b3c7828e8f3737
Chris Dalke <chrisdalke@gmail.com>
2020-02-24 00:21:10 -0500

    Get configuration loading from json and env variables

--- src/services/config/json_env_config.cpp
+++ src/services/config/json_env_config.cpp
@@ -1,12 +1,19 @@
 #include "json_env_config.h"
 #include <fmt/format.h>
 #include <unistd.h>
+#include <regex>
 #include <services/service_manager.h>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/foreach.hpp>
+#include <string>
 namespace pt = boost::property_tree;
 
 #define TELEMETRYJET_CONFIG_FILENAME ".telemetryjet"
+#define TELEMETRYJET_CONFIG_ENV_PREFIX "TELEMETRYJET_"
+
+extern char **environ;
 
 bool JsonEnvConfig::hasInt(std::string key) {
     return intValues.count(key) > 0;
@@ -65,10 +72,77 @@ void JsonEnvConfig::loadConfig(const std::string& userConfig, const std::string 
     if (!hasUserConfig && !hasLocalConfig) {
         ServiceManager::getLogger()->warning("No configuration files were loaded.");
     }
+
+    std::unordered_map<std::string, std::string> userConfigKeypairs;
+    if (hasUserConfig) {
+        BOOST_FOREACH(pt::ptree::value_type &v, propertyTreeUser) {
+                        std::string key = v.first;
+                        boost::to_lower(key);
+                        userConfigKeypairs[key] = v.second.data();
+                    }
+    }
+    ServiceManager::getLogger()->debug(fmt::format("Found {} TelemetryJet configuration parameter(s) in user configuration file.",userConfigKeypairs.size()));
+
+    std::unordered_map<std::string, std::string> localConfigKeypairs;
+    if (hasLocalConfig) {
+        BOOST_FOREACH(pt::ptree::value_type &v, propertyTreeLocal) {
+            std::string key = v.first;
+            boost::to_lower(key);
+            localConfigKeypairs[key] = v.second.data();
+        }
+    }
+    ServiceManager::getLogger()->debug(fmt::format("Found {} TelemetryJet configuration parameter(s) in local configuration file.",localConfigKeypairs.size()));
+
+    // Read configuration from all environment variables with telemetryjet prefix
+    // Currently using posix-only API, will need to add Windows solution
+    std::unordered_map<std::string, std::string> rawEnvKeypairs;
+    std::string prefix = std::string(TELEMETRYJET_CONFIG_ENV_PREFIX);
+    std::locale loc;
+    if (environ != nullptr) {
+        char *current = *environ;
+        for (int i = 1; current; i++){
+            // Check list of all environment variables and save any with telemetryjet prefix
+            std::string currentPair = std::string(current);
+            std::string key = currentPair.substr(0, currentPair.find('='));
+            std::string value = currentPair.substr(currentPair.find('=') + 1);
+            if (key.rfind(prefix,0) == 0){
+                std::string keyNoPrefix = key.substr(prefix.size());
+                boost::to_lower(keyNoPrefix);
+                rawEnvKeypairs[keyNoPrefix] = value;
+            }
+            current = *(environ+i);
+        }
+        ServiceManager::getLogger()->debug(fmt::format("Found {} TelemetryJet configuration parameter(s) in environment.",rawEnvKeypairs.size()));
+        for (const auto& pair : rawEnvKeypairs) {
+            auto key = pair.first;
+            auto val = pair.second;
+        }
+    } else {
+        ServiceManager::getLogger()->error("Environment variable configuration unavailable!");
+        ServiceManager::getLogger()->error("No environment configuration was loaded.");
+    }
+
+    // Merge keypairs
+    for (const auto& pair : userConfigKeypairs) {
+        stringValues[pair.first] = pair.second;
+    }
+    for (const auto& pair : localConfigKeypairs) {
+        stringValues[pair.first] = pair.second;
+    }
+    for (const auto& pair : rawEnvKeypairs) {
+        stringValues[pair.first] = pair.second;
+    }
+
+    // Debug: Output the merged key pairs
+    ServiceManager::getLogger()->info(fmt::format("{} configuration parameter(s) after merging.",stringValues.size()));
+    for (const auto& pair : stringValues) {
+        ServiceManager::getLogger()->debug(fmt::format(" - {}: {}",pair.first,pair.second));
+    }
 }
 
 
 JsonEnvConfig::JsonEnvConfig() {
+    ServiceManager::getLogger()->info("--------- Initializing Configuration ---------");
     char pathBuffer[1024];
     getcwd(pathBuffer,1024);
 
@@ -76,6 +150,7 @@ JsonEnvConfig::JsonEnvConfig() {
     std::string localConfigFilename = fmt::format("{}/{}",pathBuffer,TELEMETRYJET_CONFIG_FILENAME);
 
     loadConfig(userConfigFilename, localConfigFilename);
+    ServiceManager::getLogger()->info("---------- Initialized Configuration ---------");
 }
 
 JsonEnvConfig::~JsonEnvConfig() = default;
