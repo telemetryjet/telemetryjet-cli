6696276228a27eae462732f24b5b1882ecb49485
Chris Dalke <chrisdalke@gmail.com>
2020-12-01 02:12:50 -0500

    Fix typos in the ubuntu build sheel script

--- devops/package-config/linux/package-configure.sh
+++ devops/package-config/linux/package-configure.sh
@@ -13,7 +13,7 @@ cd build
 
 # Copy necessary files into manual installation bundle
 rm -rf telemetryjet-cli
-sh mkdir -p telemetryjet-cli
+mkdir -p telemetryjet-cli
 mkdir -p telemetryjet-cli/bin
 cp jet telemetryjet-cli/bin
 cp README.txt telemetryjet-cli
@@ -27,7 +27,7 @@ zip -r "telemetryjet-cli-ubuntu_x86-64_$1.zip" telemetryjet-cli/
 rm -rf telemetryjet-cli-package
 mkdir -p telemetryjet-cli-package
 mkdir -p telemetryjet-cli-package/usr/bin
-rsync -avr ../devops/package-config/linux telemetryjet-cli-package/
+rsync -avr ../devops/package-config/linux/deb/ telemetryjet-cli-package/
 find ./telemetryjet-cli-package -type d | xargs chmod 755
 objcopy jet ./telemetryjet-cli-package/usr/bin/jet --strip-debug --strip-unneeded
 gzip --best ./telemetryjet-cli-package/usr/share/doc/telemetryjet-cli/copyright
