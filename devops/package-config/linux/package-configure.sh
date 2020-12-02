a3175ede9bad6db9381eb01b9a3756b5d0d84c18
Chris Dalke <chrisdalke@gmail.com>
2020-12-02 01:57:26 +0000

    Working on repository configuration on Ubuntu

--- devops/package-config/linux/package-configure.sh
+++ devops/package-config/linux/package-configure.sh
@@ -28,6 +28,23 @@ rm -rf telemetryjet-cli-package
 mkdir -p telemetryjet-cli-package
 mkdir -p telemetryjet-cli-package/usr/bin
 rsync -avr ../devops/package-config/linux/deb/ telemetryjet-cli-package/
+tee telemetryjet-cli-package/DEBIAN/control << END
+Package: telemetryjet-cli
+Version: $1
+Section: utils
+Priority: optional
+Architecture: amd64
+Maintainer: TelemetryJet Team <telemetryjet@gmail.com>
+Homepage: https://www.telemetryjet.com/
+Depends: 
+Description: TelemetryJet CLI Information
+ The TelemetryJet CLI is a utility for monitoring and controlling
+ embedded hardware from your terminal. Pipe data from input devices
+ into files, or stream directly to a TelemetryJet server.
+ .
+ See https://docs.dev.telemetryjet.com/cli/ for documentation and guides.
+
+END
 find ./telemetryjet-cli-package -type d | xargs chmod 755
 objcopy jet ./telemetryjet-cli-package/usr/bin/jet --strip-debug --strip-unneeded
 gzip --best -n ./telemetryjet-cli-package/usr/share/doc/telemetryjet-cli/changelog
@@ -45,3 +62,14 @@ lintian telemetryjet-cli-package.deb
 mv telemetryjet-cli-package.deb "telemetryjet-cli-ubuntu-x86_64_$1.deb"
 
 cd ..
+
+# Update repository
+echo "===== UPDATING REPOSITORY ====== "
+cp "build/telemetryjet-cli-ubuntu-x86_64_$1.deb" /var/telemetryjet-downloads/builds/cli/ubuntu/apt-repository/amd64
+cd /var/telemetryjet-downloads/builds/cli/ubuntu/apt-repository/
+dpkg-sig -k telemetryjet --sign repo "amd64/telemetryjet-cli-ubuntu-x86_64_$1.deb"
+apt-ftparchive packages amd64 > Packages
+gzip -k -f Packages
+apt-ftparchive release . > Release
+rm -fr Release.gpg; gpg --default-key telemetryjet -abs -o Release.gpg Release
+rm -fr InRelease; gpg --default-key telemetryjet --clearsign -o InRelease Release
