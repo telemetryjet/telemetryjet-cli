c0ca267eaf45fce86fd6cb9077d2cd14a066d204
Chris Dalke <chrisdalke@gmail.com>
2020-12-02 02:54:14 -0500

    Fix hardcoded path in repository configure script

--- devops/package-config/linux/repository-configure.sh
+++ devops/package-config/linux/repository-configure.sh
@@ -5,7 +5,7 @@ set -e
 echo "===== UPDATING LINUX REPOSITORY FOR ARCHITECTURE $2 ====== "
 cd /var/telemetryjet-downloads/builds/cli/linux/$2/repo
 cp "../telemetryjet-cli-linux_$2_$1.deb" $2
-dpkg-sig -k telemetryjet --sign repo "amd64/telemetryjet-cli-linux_$2_$1.deb"
+dpkg-sig -k telemetryjet --sign repo "$2/telemetryjet-cli-linux_$2_$1.deb"
 apt-ftparchive packages $2 > Packages
 gzip -k -f Packages
 apt-ftparchive release . > Release
