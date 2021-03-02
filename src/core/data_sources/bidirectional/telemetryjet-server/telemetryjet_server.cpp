80a698ea1a4f5e1dfa3bdca252842d3a3a365e69
Chris Dalke <chrisdalke@gmail.com>
2021-03-02 17:43:14 -0500

    Testing hacks to fix the messagepack string encoding

--- src/core/data_sources/bidirectional/telemetryjet-server/telemetryjet_server.cpp
+++ src/core/data_sources/bidirectional/telemetryjet-server/telemetryjet_server.cpp
@@ -97,12 +97,12 @@ void TelemetryJetServerDataSource::update() {
             for (int i = 0; i < numDataPoints; i++) {
                 auto &dp = in.front();
                 in.pop_front();
-                mpack_write_cstr(&writer, apiKey.c_str());
+                mpack_write_utf8_cstr(&writer, fmt::format("{}", apiKey).c_str());
                 mpack_write_i64(&writer, dp->timestamp);
-                mpack_write_cstr(&writer, dp->key.c_str());
+                mpack_write_utf8_cstr(&writer, fmt::format("{}", dp->key).c_str());
 
                 if (dp->isStringType()) {
-                    mpack_write_cstr(&writer, dp->toString().c_str());
+                    mpack_write_utf8_cstr(&writer, fmt::format("{}", dp->toString()).c_str());
                 } else {
                     mpack_write_nil(&writer);
                 }
