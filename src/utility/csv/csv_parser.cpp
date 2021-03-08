ad6ee1368f1a98595937cf75b087f4db2c152134
NateConroy1 <nconroy@u.rochester.edu>
2021-03-07 21:58:09 -0800

    Small fix

--- src/utility/csv/csv_parser.cpp
+++ src/utility/csv/csv_parser.cpp
@@ -5,7 +5,7 @@ std::vector<std::string> parseCsvLine(const std::string& line, const std::string
 {
     // https://stackoverflow.com/a/1122720
     std::vector<std::string> output;
-    boost::tokenizer<boost::escaped_list_separator<char>> tok(line,boost::escaped_list_separator<char>('\\', separator, '\"'));
+    boost::tokenizer<boost::escaped_list_separator<char>> tok(line, boost::escaped_list_separator<char>("\\", separator, "\""));
     for (boost::tokenizer<boost::escaped_list_separator<char>>::iterator i(tok.begin()); i!=tok.end(); ++i)
     {
         output.push_back(*i);
